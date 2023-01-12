51fbf0f3eaf0dd140c2a03f6912dec19964fdcca
Tom Snelling <tomsnelling8@gmail.com>
2023-01-12 16:01:47 +0000

    add pm2 ecosystem file

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:16
-WORKDIR /app
-COPY . .
-RUN yarn install
-RUN yarn global add pm2
-EXPOSE 1337
-CMD ["pm2-runtime", "server.js"]
---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: 'strapi',
+      script: '/cnb/lifecycle/launcher',
+      args: 'yarn start'
+    }
+  ]
+}
